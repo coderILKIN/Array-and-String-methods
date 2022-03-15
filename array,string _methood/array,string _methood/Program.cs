e46c67f0afb971bad907522304659dbb92a063c6
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-15 12:23:34 +0400

    capitalized(her sozun bash herfi boyuk)

---
+++ array,string _methood/array,string _methood/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace array_string__methood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            string sentence1 = "bU tasKI metHodla yAzmAliSiz";
+
+           string lower = sentence1.ToLower();
+
+            string sentence2 = lower.Replace("b", "B").Replace("tasKI", "Taski").Replace("metHodla","Methodla").Replace("yAzmAliSiz","Yazmalisiz");
+
+            Console.WriteLine(sentence2);
+           
+
+
+        }
+
+
+    }
+}
